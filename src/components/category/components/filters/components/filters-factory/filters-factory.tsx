12336e2688e3c71d9ac3129563fe9b99013f96bc
Leroyalle <nikolajmelonov@yandex.ru>
2025-04-15 05:16:10 +0300

    feat(filters-factory): add reset to initial

--- src/components/category/components/filters/components/filters-factory/filters-factory.tsx
+++ src/components/category/components/filters/components/filters-factory/filters-factory.tsx
@@ -10,6 +10,8 @@ import {
 } from '@ant-design/icons';
 import { FiltersModal } from './components';
 import { FindAllFiltersQuery } from '@/graphql/__generated__/output';
+import { useSetAtom } from 'jotai';
+import { filtersAtom, initialFilters } from '@/store/filters-atom';
 
 interface Props {
   items: FindAllFiltersQuery['findAllFilters'];
@@ -18,6 +20,7 @@ interface Props {
 
 export const FiltersFactory: FC<Props> = ({ items, className }) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const setFilters = useSetAtom(filtersAtom);
 
   return (
     <div className={clsx('mb-4 flex flex-wrap items-center gap-2', className)}>
@@ -28,7 +31,10 @@ export const FiltersFactory: FC<Props> = ({ items, className }) => {
         <FilterOutlined size={20} />
         Фильтры
       </Button>
-      <Button variant="outlined" className="flex items-center gap-2">
+      <Button
+        variant="outlined"
+        className="flex items-center gap-2"
+        onClick={() => setFilters(initialFilters)}>
         <ArrowDownOutlined size={20} />
         По умолчанию
       </Button>
